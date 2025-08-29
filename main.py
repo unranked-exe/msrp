e795bce17299a6137aa00321e259960443ef7556
unranked-exe <120649405+unranked-exe@users.noreply.github.com>
2025-08-29 21:41:53 +0100

    refactor: Dataframe creation logic
    Created a get_datasheet() method and constants at top allowing user to specify path or name of datasheet for ease of access.

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-import pandas as pd
 from pathlib import Path
 
+import pandas as pd
 
 data_path = Path("data_store")
+datasheet_csv = data_path / "datasheet.csv"
 
-
-def main():
-    datasheet_df = pd.read_csv(data_path / "datasheet.csv")
+def main() -> None:
+    datasheet_df = get_datasheet(datasheet_csv)
     # Process the DataFrame
     print(datasheet_df.head())
 
 
-def get_datasheet():
-    return pd.read_csv(data_path / "datasheet.csv")
+def get_datasheet(data_path: Path) -> pd.DataFrame:
+    try:
+        return pd.read_csv(data_path)
+    except pd.errors.EmptyDataError as e:
+        print(f"Datasheet is empty: {e}")
+        return pd.DataFrame()  # Return an empty DataFrame on error
+    except Exception as e:
+        print(f"Error reading datasheet: {e}")
+        return pd.DataFrame()  # Return an empty DataFrame on error
 
 
 if __name__ == "__main__":
