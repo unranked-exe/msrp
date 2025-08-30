97ca23abcfc2844e87708feb72c121c04106475c
unranked-exe <120649405+unranked-exe@users.noreply.github.com>
2025-08-30 12:19:46 +0100

    chore: Added a Jupyter notebook
    Allows me to run previous code in main python file.
    This makes it easier to explore the dataset as running code in testing such as extracting a csv and conv to DF is redundant as this is not going to change after its initial run.
    This therefore makes it easier to develop

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 import pandas as pd
 
+
+
 data_path = Path("data_store")
 datasheet_csv = data_path / "datasheet.csv"
 
@@ -13,6 +15,9 @@ def main() -> None:
     print(datasheet_df["Cat Description"].value_counts())
 
     print(datasheet_df.isna().sum())
+    
+    print(datasheet_df.duplicated().sum())
+    print(datasheet_df[datasheet_df[["STYLE Description","GBP G - MSRP"]].duplicated()])
 
 
 def get_datasheet(data_path: Path) -> pd.DataFrame:
