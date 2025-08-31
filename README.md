6dd4ee7051a473b3ef48ac8a4261274d6a828311
unranked-exe <120649405+unranked-exe@users.noreply.github.com>
2025-08-31 02:06:40 +0100

    CHANGED PROJECT AIM
    Going to scrape a new retailer for products and compare prices with others.
    
    Planning to use @Pydantic to help with data modelling of JSON objects from requests to api.
    
    Planning to use @rnet library to perform requests to api due to better impersonation techniques compared to Selenium/Playright

---
+++ README.md
@@ -0,0 +1,3 @@
+Web Scraper that compares prices of items from a brand with prices listed on retailer websites.
+
+Used tools like [technical seo](https://technicalseo.com/tools/robots-txt/) to check the scraaping urls against websites robot.txt for reposible scraping
--- main.py
+++ main.py
@@ -1,21 +1,37 @@
+import asyncio
+import os
 from pathlib import Path
 
 import pandas as pd
+from pydantic import BaseModel
+from rnet import Client, Impersonate
 
 data_path = Path("data_store")
 datasheet_csv = data_path / "datasheet.csv"
 
-def main() -> None:
-    datasheet_df = get_datasheet(datasheet_csv)
-    # Process the DataFrame
-    print(datasheet_df.head())
+# FOR SCRAPING Manufacturer Website
+# Check for required environment variables
+try:
+    PRODUCT_API_URL = os.environ.get("PRODUCT_API_URL")
+    SEARCH_QUERY_URL = os.environ.get("SEARCH_QUERY_URL")
 
-    print(datasheet_df["Cat Description"].value_counts())
+except KeyError as e:
+    raise ValueError(f"Missing environment variable: {e}")
 
-    print(datasheet_df.isna().sum())
 
-    print(datasheet_df.duplicated().sum())
-    print(datasheet_df[datasheet_df[["STYLE Description","GBP G - MSRP"]].duplicated()])
+#DATA MODEL FOR SHOES
+class Product(BaseModel):
+    sku: str
+    name: str
+    price: float
+    image_url: str
+    product_url: str
+
+class SearchResults(BaseModel):
+    products: list[Product]
+    count: int
+    page: int
+    page_size: int
 
 
 def get_datasheet(data_path: Path) -> pd.DataFrame:
@@ -27,6 +43,24 @@ def get_datasheet(data_path: Path) -> pd.DataFrame:
     except Exception as e:
         print(f"Error reading datasheet: {e}")
         return pd.DataFrame()  # Return an empty DataFrame on error
+
+def create_client() -> Client:
+    return Client(impersonate=Impersonate.Chrome137)
+
+
+# TO REDO AND REDESIGN WITH NEW PROJECT IDEA
+def main() -> None:
+    datasheet_df = get_datasheet(datasheet_csv)
+    # Process the DataFrame
+    # print(datasheet_df.head())
+
+    # print(datasheet_df["Cat Description"].value_counts())
+
+    # print(datasheet_df.isna().sum())
+
+    # print(datasheet_df.duplicated().sum())
+    # print(datasheet_df[datasheet_df[["STYLE Description","GBP G - MSRP"]].duplicated()])
+
 
 
 if __name__ == "__main__":
