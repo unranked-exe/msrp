2d6abace3febea8e05e9a3afba27b3d4f3ec9625
unranked-exe <120649405+unranked-exe@users.noreply.github.com>
2025-08-31 21:06:40 +0100

    refactor & feat: Access methods to APIs, Altered Data Schema
    Added methods to access API for products.
    However I saved some of the responses I got before as a JSON for development testing of my Pydantic Data Schemas. This is to reduce chance of being blocked for excess requests.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# MSRP
+
 Web Scraper that compares prices of items from a brand with prices listed on retailer websites.
 
-Used tools like [technical seo](https://technicalseo.com/tools/robots-txt/) to check the scraaping urls against websites robot.txt for reposible scraping
+Used tools like [technical seo](https://technicalseo.com/tools/robots-txt/) to check the scraping urls against websites robot.txt for reposible scraping
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
+import json
 import os
 from pathlib import Path
+from urllib.parse import urlencode
 
 import pandas as pd
 from pydantic import BaseModel
@@ -13,26 +15,33 @@ datasheet_csv = data_path / "datasheet.csv"
 # Check for required environment variables
 try:
     PRODUCT_API_URL = os.environ.get("PRODUCT_API_URL")
-    SEARCH_QUERY_URL = os.environ.get("SEARCH_QUERY_URL")
+    SEARCH_QUERY_BASE_URL = os.environ.get("SEARCH_QUERY_BASE_URL")
 
 except KeyError as e:
     raise ValueError(f"Missing environment variable: {e}")
 
 
 #DATA MODEL FOR SHOES
-class Product(BaseModel):
-    sku: str
-    name: str
+class SearchItem(BaseModel):
+    productId: str  # noqa: N815
+    displayName: str  # noqa: N815
+    division: str
     price: float
-    image_url: str
-    product_url: str
 
 class SearchResults(BaseModel):
-    products: list[Product]
     count: int
-    page: int
-    page_size: int
+    startIndex: int  # noqa: N815
+    searchTerm: str  # noqa: N815
+    items: list[SearchItem]
 
+
+def prepare_query() -> str:
+    # Prepare the search query
+    params = {
+        "multi_age_gender": "men",
+        "v_size_en_gb": "10",
+    }
+    return f"{SEARCH_QUERY_BASE_URL}&{urlencode(params)}"
 
 def get_datasheet(data_path: Path) -> pd.DataFrame:
     try:
@@ -44,13 +53,40 @@ def get_datasheet(data_path: Path) -> pd.DataFrame:
         print(f"Error reading datasheet: {e}")
         return pd.DataFrame()  # Return an empty DataFrame on error
 
+def get_json(data_path: Path) -> dict:
+    try:
+        with Path.open(data_path / "search_res.json") as f:
+            return json.load(f)
+    except FileNotFoundError as e:
+        print(f"JSON file not found: {e}")
+        return {}
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+        return {}
+
 def create_client() -> Client:
     return Client(impersonate=Impersonate.Chrome137)
 
+async def search_api(client: Client) -> None:
+    # THIS IS TO PREVENT EXCESS QUERIES BEING SENT TO API SO SAVED JSON LOCALLY ON FILE
+    #response = await client.get(SEARCH_QUERY_URL)
+    #print(response.status_code)
+    #print(response.json())
+    json_res = get_json(data_path)
+    print(json_res)
+    print(json_res["raw"]["itemList"])
+    return SearchResults(**json_res["raw"]["itemList"])
+
+async def product_api(client: Client, search_item: SearchItem) -> None:
+    url = f"{PRODUCT_API_URL}{search_item.productId}"
+    response = await client.get(url)
+    print(response.status_code)
+    print(response.json())
+
 
 # TO REDO AND REDESIGN WITH NEW PROJECT IDEA
-def main() -> None:
-    datasheet_df = get_datasheet(datasheet_csv)
+async def main() -> None:
+    #datasheet_df = get_datasheet(datasheet_csv)
     # Process the DataFrame
     # print(datasheet_df.head())
 
@@ -61,7 +97,15 @@ def main() -> None:
     # print(datasheet_df.duplicated().sum())
     # print(datasheet_df[datasheet_df[["STYLE Description","GBP G - MSRP"]].duplicated()])
 
+    client = create_client()
+    search_results = await search_api(client)
+    product_api(client, search_results.items[0])
 
 
 if __name__ == "__main__":
-    main()
+    #run this for testing
+    #await main()
+    print(PRODUCT_API_URL)
+    print(prepare_query())
+    asyncio.run(main())
+    print(PRODUCT_API_URL)
